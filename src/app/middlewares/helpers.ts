fee303cf04d7ae812f4bcef8b452516ed8d8ff61
ziyad-bit <ziyad199523@gmail.com>
2026-03-17 11:05:03 +0200

    mvc

--- src/app/middlewares/helpers.ts
+++ src/app/middlewares/helpers.ts
@@ -1,25 +1,17 @@
 /* eslint-disable no-unused-vars */
-import { Request, Response, NextFunction } from "express";
+import { NextFunction, Request, Response } from "express";
 import { cookieConfig } from "../../config/cookie.ts";
 
 declare global {
     namespace Express {
         interface Response {
-            cookieHelper: (
-                name: string,
-                value: string,
-                maxAge: number,
-            ) => Response;
+            cookieHelper: (name: string, value: string, maxAge: number) => Response;
         }
     }
 }
 
-export const attachHelpers = (
-    req: Request,
-    res: Response,
-    next: NextFunction,
-): void => {
-    res.cookieHelper = (name: string, value: string, maxAge: number) => {
+export const attachHelpers = (req: Request, res: Response, next: NextFunction): void => {
+    res.cookieHelper = (name: string, value: string, maxAge: number): Response => {
         return res.cookie(name, value, cookieConfig(maxAge));
     };
 
