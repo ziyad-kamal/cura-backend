d814c9ec2fcd7d68897fb40ddbd4835ac377b0b0
ziyad-bit <ziyad199523@gmail.com>
2026-05-09 10:52:28 +0300

    fix-errors3

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,10 +1,8 @@
 import cors from "cors";
 
-export const applyCors = () => {
-    cors({
-        origin: process.env.CLIENT_URL || "http://localhost:5173",
-        credentials: true,
-        methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-        allowedHeaders: ["Content-Type", "Authorization"],
-    });
-};
+export const applyCors = cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+});
