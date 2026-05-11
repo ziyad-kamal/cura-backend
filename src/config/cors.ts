385fa36308653826c17ef4d2ac2c07e7918ad4f3
ziyad-bit <ziyad199523@gmail.com>
2026-05-11 19:36:30 +0300

    fix-cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -4,23 +4,16 @@ const allowedOrigins = ["http://localhost:5173", "http://localhost:3000", proces
     Boolean,
 ) as string[];
 
-// export const applyCors = cors({
-//     origin: (origin, callback) => {
-//         if (!origin) return callback(null, true);
-
-//         if (allowedOrigins.includes(origin)) {
-//             callback(null, true);
-//         } else {
-//             callback(new Error(`CORS blocked: ${origin}`));
-//         }
-//     },
-//     credentials: true,
-//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-// });
-
 export const applyCors = cors({
-    origin: process.env.NODE_ENV === "production" ? allowedOrigins : (origin, callback) => callback(null, true),
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+
+        if (allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error(`CORS blocked: ${origin}`));
+        }
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
