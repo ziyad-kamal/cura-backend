9dc05558398c8e7faf5510dac00c3cc8550fb5bb
ziyad-bit <ziyad199523@gmail.com>
2026-05-21 15:59:03 +0300

    edit-tsconfig

--- src/index.ts
+++ src/index.ts
@@ -25,12 +25,11 @@ import orderItemRoutes from "./routes/marketplace/orderItemRoutes.js";
 import reviewRoutes from "./routes/marketplace/reviewRoutes.js";
 import repostRoutes from "./routes/users/repostRoutes.js";
 
+connectDB();
 
 const app = express();
 
 async function bootstrap() {
-    await connectDB();
-
     app.listen(appConfig.port);
 
     app.use(applyCors);
