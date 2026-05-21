f99d670fd886cd89f7181a25f35d593fb67b876a
ziyad-bit <ziyad199523@gmail.com>
2026-05-21 16:46:49 +0300

    t

--- src/routes/users/postRoutes.ts
+++ src/routes/users/postRoutes.ts
@@ -1,11 +1,10 @@
 import express from "express";
-import { destroy, like,  store,  update } from "../../app/controllers/users/postController.js";
+import { destroy, index, like,  store,  update } from "../../app/controllers/users/postController.js";
 import { validateId } from "../../app/middlewares/validateId.js";
 import { postValidator } from "../../app/validators/postValidator.js";
 import { jwtVerify } from "./../../app/middlewares/jwtVerify.js";
 import { authorize } from "../../app/middlewares/authorize.js";
 import Post from "../../app/models/Post.js";
-import { index } from "@/app/controllers/users/commentController.js";
 
 const postRoutes = express.Router();
 
