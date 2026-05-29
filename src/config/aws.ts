71f29b2fad1455e8b4ef82fa02d249b08594f526
ziyad-bit <ziyad199523@gmail.com>
2026-05-29 18:36:11 +0300

    update-aws-config

--- src/config/aws.ts
+++ src/config/aws.ts
@@ -4,13 +4,13 @@ import { S3Client } from "@aws-sdk/client-s3";
 
 export const awsConfig: AwsInterface = {
     s3_bucket_name: process.env.S3_BUCKET_NAME || "",
-    region: process.env.REGION_DEV|| "",
+    region: process.env.REGION|| "",
 };
 
 export const s3Client = new S3Client({
-    region: process.env.REGION_DEV,
+    region: process.env.REGION,
     credentials: {
-        accessKeyId: process.env.ACCESS_KEY_ID_DEV || "",
+        accessKeyId: process.env.ACCESS_KEY_ID || "",
         secretAccessKey: process.env.SECRET_KEY || "",
     },
 });
