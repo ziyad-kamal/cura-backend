18e24621f7c911372b20286185547d09a8457596
ziyad-bit <ziyad199523@gmail.com>
2026-06-05 14:16:25 +0300

    chat

--- src/app/middlewares/jwtVerify.ts
+++ src/app/middlewares/jwtVerify.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import jwt, { JwtPayload, SignOptions } from "jsonwebtoken";
+import jwt, { SignOptions } from "jsonwebtoken";
 import { jwtConfig } from "../../config/jwt.js";
 import { returnError } from "../utils/returnJson.js";
 
