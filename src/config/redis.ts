a418c0ddaf4ec6b670d57a05114c7f6665e53c98
Ali Algohary <71212887+alimelgohary@users.noreply.github.com>
2026-06-15 13:26:43 +0300

    Fix redis connect to elasticache

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -5,6 +5,7 @@ import "dotenv/config";
 export const redis = new Redis({
     host: process.env.REDIS_HOST || "localhost",
     port: Number(process.env.REDIS_PORT) || 6379,
+    tls: process.env.REDIS_HOST?.includes("amazonaws.com") ? {} : undefined,
     maxRetriesPerRequest: null,
 });
 
