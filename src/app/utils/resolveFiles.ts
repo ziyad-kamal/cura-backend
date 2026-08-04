f9efd88f50f71f7aa8f98e7303cd83b573c78130
ziyad-bit <ziyad199523@gmail.com>
2026-08-05 02:40:43 +0300

    t

--- src/app/utils/resolveFiles.ts
+++ src/app/utils/resolveFiles.ts
@@ -6,7 +6,6 @@ export const resolveFiles = async <T extends { s3Key: string }>(
 ): Promise<(T & { url: string })[]> => {
     if (!files?.length) return [];
 
-    
     if (visibility === "public") {
         // return files
         //     .filter((file) => file.s3Key)
